abb5cff629a3ea9e50ccb733032e90c73a4ef6c7
Goce <melancholydive@gmail.com>
2021-12-07 20:05:36 +0100

    broken homepage into smaller components and finished styling our menu-item

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+//exact same thing as writing props.title/or pulling any data from props
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
